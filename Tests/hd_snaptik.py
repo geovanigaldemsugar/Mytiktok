f3325912d363207d2c73e746da684b202f2ffba7
geo <geovaninotice@gmail.com>
2024-05-03 02:33:11 -0500

    removed test from cache

--- Tests/hd_snaptik.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-url = 'https://snaptik.app/getHdLink.php?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpZCI6IjczNTAzODc4NjAxNTI5MTMxOTgifQ.bAnJxIr1XVxWtGPLPUB89akMRln-n2rGH28xTcZ5Dy4'
-
-response  = requests.get(url)
-print(response.json())
--- Tests/video_test.py
+++
@@ -1,8 +0,0 @@
-from src.mytiktok.video import Video
-
-
-
-url = 'https://www.tiktok.com/@calebbpartain/video/7363384781024906538'
-
-video  = Video(url = url)
-video.download(file_name='test.mp4')
