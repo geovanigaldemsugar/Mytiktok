29d73c8bc06b677494bb3b14fcfe8b0153349959
geo <geovaninotice@gmail.com>
2024-05-03 20:13:00 -0500

    changed toml file

--- test_tiktok.py
+++ test_tiktok.py
@@ -1,8 +1,7 @@
 from src.mytiktok import Tiktok
 from dotenv import load_dotenv
 import os
-
-
+from src.mytiktok import helper
 #load env variables in environment
 load_dotenv(dotenv_path='my_env.env')
 
@@ -12,5 +11,8 @@ password = os.getenv('PASSWORD')
 # email = 'sadasdas@gmail.co,'
 # password = '2efwfwfw'
 
-tiktok  = Tiktok(email=email, password=password).search(search_term='asdasda', amnt=4)
+print(helper.cookies_path)
+
+
+# tiktok  = Tiktok(email=email, password=password).search(search_term='asdasda', amnt=4)
                                         
