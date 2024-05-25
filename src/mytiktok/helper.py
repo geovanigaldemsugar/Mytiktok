a8aa08465349d1a3811e877ef21cbc065205b42f
geovanigaldemsugar <67174852+geovanigaldemsugar@users.noreply.github.com>
2024-05-24 20:42:56 -0500

    Update helper.py

--- src/mytiktok/helper.py
+++ src/mytiktok/helper.py
@@ -83,7 +83,7 @@ class Helper():
 
         elif SYSTEM == 'Linux' :
             browser_url = 'https://storage.googleapis.com/chrome-for-testing-public/125.0.6422.76/linux64/chrome-linux64.zip'
-            driver_url  = '	https://storage.googleapis.com/chrome-for-testing-public/125.0.6422.76/linux64/chromedriver-linux64.zip'
+            driver_url  = 'https://storage.googleapis.com/chrome-for-testing-public/125.0.6422.76/linux64/chromedriver-linux64.zip'
 
         # parse the names from the urls and create thier paths
         browser_file_name = browser_url.split('/')[-1]
